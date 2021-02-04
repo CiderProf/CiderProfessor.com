bd272ed4696f54aa32e9ef1eccb5dc2b63fedf1d
CiderProf <theciderprof@gmail.com>
2021-02-03 21:34:08 -0800

    nav bar function complete, begin styling cider page

--- CiderProf/public/app.js
+++ CiderProf/public/app.js
@@ -1,3 +1,35 @@
-$('#ciderhouse').on('click', function() {
-    $(this).next().removeClass('cideries');
-  });
+$(function(){
+
+
+$("#gradebook").click( function() {
+  $(".gradebook").toggleClass("hide");
+});
+
+$("#gradebook ul").click( function(e){
+  let $this = e.target.children;
+  if($this && $this.length > 0){
+    $(e.target.children).toggleClass("hide");    
+  }
+  $(this).toggleClass("hide");
+});
+
+$(".style").click( function() {
+  $(this).next().children().toggleClass("hide");
+  $(this).parent().toggleClass("hide");
+});
+
+$("#complit").click( function() {
+  $(".complit").toggleClass("hide");
+});
+
+$("#cidreviews").click( function() {
+  $(".cidreviews").toggleClass("hide");
+});
+
+$("#crgeography").click( function() {
+  $(".crgeography").toggleClass("hide");
+  $(this).parent().toggleClass("hide");
+});
+
+
+});
